ab6be5581da6b39cebac8935ac4d56c210e267ed
Em Rea <36008259+ReaCastGames@users.noreply.github.com>
2025-01-26 18:39:00 -0600

    caps

--- Godot project/src/Flotsam/FlotsamItems/Capsule/Capsule.cs
+++ Godot project/src/Flotsam/FlotsamItems/Capsule/Capsule.cs
@@ -3,19 +3,25 @@ using Godot;
 
 public partial class Capsule : Node3D {
 
+	[Export] public MeshInstance3D cap;
 	[Export] public Node3D toyNode;
 	[Export] public PackedScene[] itemMeshes;
+	public Color capColor;
 
 	public override void _Ready() {
 		var newToy = itemMeshes[(new RandomNumberGenerator()).RandiRange(0, itemMeshes.Length - 1)];
 		var newToyNode = newToy.Instantiate();
 		((Node3D)newToyNode).Position = toyNode.Position;
 		var parent = GetParent();
-		newToyNode.CallDeferred("name", "Toy");
+		//newToyNode.CallDeferred("name", "Toy");
 		parent.CallDeferred("add_child", newToyNode);
 		parent.CallDeferred("remove_child", toyNode);
-		// parent.AddChild(newToyNode);
-		// parent.RemoveChild(toyNode);
+
+		var material = (BaseMaterial3D)cap.GetSurfaceOverrideMaterial(0);
+		Color[] colorsArray = [Colors.Yellow, Colors.Teal, Colors.Magenta];
+		capColor = colorsArray[new RandomNumberGenerator().RandiRange(0, colorsArray.Length - 1)];
+		material.AlbedoColor = capColor;
+		cap.SetSurfaceOverrideMaterial(0, material);
 	}
 
 	public override void _Process(double delta) {
